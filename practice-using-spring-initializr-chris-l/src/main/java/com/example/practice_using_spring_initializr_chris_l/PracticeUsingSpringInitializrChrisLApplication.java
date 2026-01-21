40e089bc4653ff55e07ebf415a9ead237fa824d1
cmlorbert <cmlorbert@gmail.com>
2026-01-21 13:38:19 -0600

    added spring files

---
+++ practice-using-spring-initializr-chris-l/src/main/java/com/example/practice_using_spring_initializr_chris_l/PracticeUsingSpringInitializrChrisLApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_using_spring_initializr_chris_l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeUsingSpringInitializrChrisLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeUsingSpringInitializrChrisLApplication.class, args);
+	}
+
+}
---
+++ practice-using-spring-initializr-chris-l/src/test/java/com/example/practice_using_spring_initializr_chris_l/PracticeUsingSpringInitializrChrisLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_using_spring_initializr_chris_l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeUsingSpringInitializrChrisLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
